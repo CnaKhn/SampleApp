99c9eb4d4d9739969bd15877d3e78407f5f189a5
CnaKhn <sinakhanbabaie@gmail.com>
2021-05-03 14:31:53 +0430

    learning-DYNAMIC-Receiver

--- app/src/main/java/com/example/sample_app/BroadcastReceiver/BroadcastReceiverActivity.java
+++ app/src/main/java/com/example/sample_app/BroadcastReceiver/BroadcastReceiverActivity.java
@@ -2,15 +2,31 @@ package com.example.sample_app.BroadcastReceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 
 import com.example.sample_app.R;
 
 public class BroadcastReceiverActivity extends AppCompatActivity {
+    ExampleBroadcastReceiver broadcastReceiver = new ExampleBroadcastReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broadcast_receiver);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(broadcastReceiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastReceiver);
     }
 }
