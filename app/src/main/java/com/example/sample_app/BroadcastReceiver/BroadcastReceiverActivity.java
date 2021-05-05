f83caa2962d98b2a9e0af64255f83acb4f2b8abe
CnaKhn <sinakhanbabaie@gmail.com>
2021-05-05 16:46:35 +0430

    practicing-broadcast-receivers

--- app/src/main/java/com/example/sample_app/BroadcastReceiver/BroadcastReceiverActivity.java
+++ app/src/main/java/com/example/sample_app/BroadcastReceiver/BroadcastReceiverActivity.java
@@ -9,27 +9,11 @@ import android.os.Bundle;
 import com.example.sample_app.R;
 
 public class BroadcastReceiverActivity extends AppCompatActivity {
-    //ExampleBroadcastReceiver broadcastReceiver = new ExampleBroadcastReceiver();
-    OrderedReceiver1 orderedReceiver1 = new OrderedReceiver1();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broadcast_receiver);
-        /*IntentFilter filter = new IntentFilter("com.example.sample_app.EXAMPLE_ACTION");
-        registerReceiver(broadcastReceiver, filter);*/
-        IntentFilter filter = new IntentFilter("com.example.sample_app.EXAMPLE_ACTION");
-        registerReceiver(orderedReceiver1, filter);
+
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(orderedReceiver1);
-    }
-
-    /*@Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(broadcastReceiver);
-    }*/
 }
